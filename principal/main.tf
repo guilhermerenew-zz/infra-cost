d24b379f227438e6dca5c686a3d9fe158d8f5dc8
guilhermerenew <guilhermeoliveira2862@hotmail.com>
2021-10-23 21:21:39 -0300

    trassssssh

--- principal/main.tf
+++ principal/main.tf
@@ -17,7 +17,7 @@ resource "aws_instance" "web_app" {
     device_name = "my_data"
     volume_type = "io1"                     # <<<<< Try changing this to gp2 to compare costs
     volume_size = 3000
-    iops        = 1500
+    iops        = 1200
   }
 }
 
