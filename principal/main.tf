4a1f5ca8a99abc2ceaa2813787a539d2f950b5a0
guilhermerenew <guilhermeoliveira2862@hotmail.com>
2022-05-24 00:44:46 -0300

    GCP automate forced checks!

--- principal/main.tf
+++ principal/main.tf
@@ -1,343 +1,39 @@
-# Cloud Provider Access
-provider "aws" {
-  region                      = "us-east-1"
-  skip_credentials_validation = true
-  skip_requesting_account_id  = true
+provider "google" {
+ region      = "us-central1"
 }
 
-# Setting up VPC
-resource "aws_vpc" "mw_vpc" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_support   = true
-  enable_dns_hostnames = true
-  tags = {
-    Name = "MediaWikiVPC"
-  }
-}
+resource "google_compute_instance" "instance1" {
+ name         = "instance1"
+ machine_type = "n1-standard-32" # <<<<< Try changing this to n1-standard-8 to compare the costs
+ zone         = "us-central1-a"
 
-# Gateway for Internet connection
-resource "aws_internet_gateway" "mw_igw" {
-  vpc_id = aws_vpc.mw_vpc.id
-  tags = {
-    Name = "Gateway para internet"
-  }
-}
-
-# Creation Route Tables
-resource "aws_route_table" "mw_rt" {
-  vpc_id = aws_vpc.mw_vpc.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.mw_igw.id
-  }
-    tags = {
-      Name = "Rota-MediaWiki"
-    }
-}
-
-# Routing Tables Associations
-resource "aws_route_table_association" "public-az1" {
-  subnet_id      = aws_subnet.mw_subnet1.id
-  route_table_id = aws_route_table.mw_rt.id
-}
-
-resource "aws_route_table_association" "public-az2" {
-  subnet_id      = aws_subnet.mw_subnet2.id
-  route_table_id = aws_route_table.mw_rt.id
-}
-
-resource "aws_route_table_association" "public-az3" {
-  subnet_id      = aws_subnet.mw_subnet3.id
-  route_table_id = aws_route_table.mw_rt.id
-}
-
-# subnet1 - ap-northeast-1a
-resource "aws_subnet" "mw_subnet1" {
-  vpc_id            = aws_vpc.mw_vpc.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "ap-northeast-1a"
-  tags = {
-    Name = "MediaWikiSubnet1"
-  }
-}
-
-# subnet2 - ap-northeast-1b
-resource "aws_subnet" "mw_subnet2" {
-  vpc_id            = aws_vpc.mw_vpc.id
-  cidr_block        = "10.0.2.0/24"
-  availability_zone = "ap-northeast-1c"
-  tags = {
-    Name = "MediaWikiSubnet2"
-  }
-}
-
-# Subnet3 - ap-northeast-1c
-resource "aws_subnet" "mw_subnet3" {
-  vpc_id            = aws_vpc.mw_vpc.id
-  cidr_block        = "10.0.3.0/24"
-  availability_zone = "ap-northeast-1d"
-  tags = {
-    Name = "MediaWikiSubnet3"
-  }
-}
-
-#Creation Security Group mw_sg
-resource "aws_security_group" "mw_sg" {
-  name        = "mw_sg"
-  description = "Connection http, ssh, mysql, api-rest wiki and Grafana"
-  vpc_id      = aws_vpc.mw_vpc.id
-  tags = {
-    "Name" = "mw_sg"
-  }
-
-# ingress port 22/ssh
-  ingress {
-    from_port   = 22 
-    to_port     = 22
-    protocol    = "TCP"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-# ingress port 80/http
-   ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "TCP"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-# ingress port 3306 for mysql
-  ingress {
-    from_port   = 3306
-    to_port     = 3306
-    protocol    = "TCP"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-# ingress port 3000 for grafana Connection
-  ingress {
-    from_port   = 3000
-    to_port     = 3000
-    protocol    = "TCP"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-# ingress port 5000 for Connection api-rest (docker app)
-  ingress {
-    from_port   = 5000
-    to_port     = 5000
-    protocol    = "TCP"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-# ICMP ingress
-  ingress {
-    from_port   = "0"
-    to_port     = "0"
-    protocol    = "icmp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-# egress for All Connections
-  egress {
-    from_port   = "0"
-    to_port     = "0"
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-# Creating ElasticLoadBalancer 
-resource "aws_elb" "mw_elb" {
-  name              = "media-terraform-elb"
-  subnets           = ["${aws_subnet.mw_subnet1.id}", "${aws_subnet.mw_subnet2.id}"]
-  security_groups   = ["${aws_security_group.mw_sg.id}"]
-  listener {
-    instance_port     = 80
-    instance_protocol = "http"
-    lb_port           = 80
-    lb_protocol       = "http"
-  }
-  listener {
-    instance_port     = 5000
-    instance_protocol = "http"
-    lb_port           = 5000
-    lb_protocol       = "http"
-  }
-  instances       = ["${aws_instance.webserver1.id}", "${aws_instance.webserver2.id}"]
-  tags = {
-    Name = "media-terraform-elb"
-  }
-}
-
-#Creation of EC2 instances for mediawiki Application
-# App webserver1 Instance
-resource "aws_instance" "webserver1" {
-  security_groups             = [aws_security_group.mw_sg.id]
-  ami                         = "ami-07dd14faa8a17fb3e"
-  instance_type               = "t2.nano"
-  key_name                    = "terraform-aws"
-  subnet_id                   = aws_subnet.mw_subnet1.id
-  private_ip                  = "10.0.1.10"
-  associate_public_ip_address = true
-  tags = {
-    Name  = "mediawiki-web1"
-    group = "web"
-    app   = "Application"
-  }
-}
-
-# App webserver2 Instance
-resource "aws_instance" "webserver2" {
-  security_groups             = [aws_security_group.mw_sg.id]
-  ami                         = "ami-07dd14faa8a17fb3e"
-  instance_type               = "t2.nano"
-  key_name                    = "terraform-aws"
-  subnet_id                   = aws_subnet.mw_subnet2.id
-  private_ip                  = "10.0.2.20"
-  associate_public_ip_address = true
-  tags = {
-    Name  = "mediawiki-web2"
-    group = "web"
-    app   = "Application"
-  }
-}
-
-# Grafana Instance (Monitoring)
-resource "aws_instance" "grafana" {
-  security_groups             = [aws_security_group.mw_sg.id]
-  ami                         = "ami-07dd14faa8a17fb3e"
-  instance_type               = "c5d.metal"
-  key_name                    = "terraform-aws"
-  subnet_id                   = aws_subnet.mw_subnet3.id
-  private_ip                  = "10.0.3.30"
-  associate_public_ip_address = true
-  tags = {
-    Name  = "mediawiki-grafana"
-    group = "web"
-    app   = "Monitoring"
-  }
-}
-
-# Creating Subnet aws_db and associating application subnets
-resource "aws_db_subnet_group" "bd" {
-  name        = "rds"
-  description = "Private subnets for RDS instance"
-  subnet_ids  = [aws_subnet.mw_subnet1.id, aws_subnet.mw_subnet2.id, aws_subnet.mw_subnet3.id]
-  tags = {
-    Name = "My DB subnet group"
-  }
-}
-
-# Security Group for DB Instance
-resource "aws_security_group" "security-group-rds" {
-    name        = "security-group-rds"
-    description = "RDS security group"
-    vpc_id      = aws_vpc.mw_vpc.id
-    ingress {
-      from_port   = 22
-      to_port     = 22
-      protocol    = "tcp"
-      cidr_blocks = [aws_vpc.mw_vpc.cidr_block]
+ boot_disk {
+   initialize_params {
+     image = "debian-cloud/debian-9"
    }
-    ingress {
-      from_port   = 3306
-      to_port     = 3306
-      protocol    = "TCP"
-      cidr_blocks = ["0.0.0.0/0"]
-  }
-    egress {
-      from_port   = 0
-      to_port     = 0
-      protocol    = "-1"
-      cidr_blocks = ["0.0.0.0/0"]
-  }
-  tags = {
-    "Name" = "security-group-rds"
-  }
-}
+ }
 
-# Creating RDS Instance
-resource "aws_db_instance" "wikidatabase" {
-  allocated_storage       = 5
-  max_allocated_storage   = 10
-  storage_type            = "gp2"
-  engine                  = "mysql"
-  engine_version          = "5.7"
-  instance_class          = "db.m6g.2xlarge"
-  name                    = "wikidatabase"
-  username                = "wiki"
-  password                = "wik987%$"
-  port                    = 3306
-  identifier              = "wikidatabase"
-  parameter_group_name    = "default.mysql5.7"
-  skip_final_snapshot     = true
-  publicly_accessible     = true
-  copy_tags_to_snapshot   = true
-  maintenance_window      = "Mon:06:00-Mon:09:00"
-  backup_window           = "09:01-11:00"
-  backup_retention_period = 7
-  db_subnet_group_name    = aws_db_subnet_group.bd.id
-  vpc_security_group_ids  = [aws_security_group.security-group-rds.id]
-  tags = {
-    name        = "wikidatabase"
-    application = "production"
+ scheduling {
+   preemptible = true
+ }
+
+  guest_accelerator {
+    type = "nvidia-tesla-t4" # <<<<< Try changing this to nvidia-tesla-p4 to compare the costs
+    count = 4
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+    }
   }
 }
 
-# Creating Replica RDS in ap-northeast-1d
-resource "aws_db_instance" "wikidatabase-replica" {
-  identifier              = "wikidatabase-replica"
-  replicate_source_db     = aws_db_instance.wikidatabase.id
-  allocated_storage       = 5
-  max_allocated_storage   = 10
-  availability_zone       = "ap-northeast-1d"
-  engine                  = "mysql"
-  engine_version          = "5.7"
-  instance_class          = "db.t2.micro"	
-  storage_type            = "gp2"
-  username                = "wiki"
-  password                = "wik987%$"
-  port                    = 3306
-  parameter_group_name    = "default.mysql5.7"
-  maintenance_window      = "Mon:06:00-Mon:09:00"
-  backup_window           = "09:01-11:00"
-  backup_retention_period = 0
-  skip_final_snapshot     = true
-  publicly_accessible     = true
-  copy_tags_to_snapshot   = true
-  multi_az                = false
-  apply_immediately       = true
-  tags = {
-    name        = "wikidatabase-replica"
-    application = "production"
-  }
-}
-
-# Create zone!
-resource "aws_route53_zone" "area-zone" {
-  name = "applicationdesktop.cf"
-}
-
-# Record name!
-resource "aws_route53_record" "database-record" {
-  zone_id = aws_route53_zone.area-zone.zone_id
-  name    = "database.${aws_route53_zone.area-zone.name}"
-  type    = "CNAME"
-  ttl     = 30
-  records = ["${aws_db_instance.wikidatabase.address}"]
-}
-
-#Function Lambda test!
-resource "aws_lambda_function" "hello_world-gui" {
-  function_name = "hello_world-gui"
-  role          = "arn:aws:lambda:us-east-1:account-id:resource-id"
-  handler       = "exports.test"
-  runtime       = "nodejs12.x"
-  memory_size   = 512
-}
-
-# ElasticLoadBalancer Addres for Connections
-output "address" {
-  value = aws_elb.mw_elb.dns_name
+resource "google_dns_record_set" "frontend" {
+  name = "frontend.123"
+  type = "A"
+  ttl  = 300
+  rrdatas = ["123.123.123.123]"]
+  managed_zone = "zone"
 }
