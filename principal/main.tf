1683bcda421a66c3f6649a21d6484fb986bbef24
guilhermerenew <guilhermeoliveira2862@hotmail.com>
2022-05-25 17:46:47 -0300

    update values for testing :rocket:

--- principal/main.tf
+++ principal/main.tf
@@ -4,25 +4,17 @@ provider "google" {
   zone        = "us-central1-c"
 }
 
-resource "google_compute_instance" "instance1" {
- name         = "instance1"
- machine_type = "n1-standard-32" # <<<<< Try changing this to n1-standard-8 to compare the costs
- zone         = "us-central1-a"
+resource "google_compute_instance" "instance-diff" {
+ name         = "instance-diff"
+ machine_type = "n1-standard-8"
+ count        = 100
+ zone         = "us-central1-a" 
 
  boot_disk {
    initialize_params {
      image = "debian-cloud/debian-9"
    }
  }
-
- scheduling {
-   preemptible = true
- }
-
-  guest_accelerator {
-    type = "nvidia-tesla-v100" # <<<<< Try changing this to nvidia-tesla-p4 to compare the costs
-    count = 1
-  }
 
   network_interface {
     network = "default"
