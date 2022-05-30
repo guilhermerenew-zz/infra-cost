6786ff2e37bd4a71bfb163fee7c1bf86fbc013ce
guilhermerenew <guilhermeoliveira2862@hotmail.com>
2022-05-29 21:36:23 -0300

    new machine filed!

--- principal/main.tf
+++ principal/main.tf
@@ -7,8 +7,28 @@ provider "google" {
 resource "google_compute_instance" "instance-diff" {
  name         = "instance-diff"
  machine_type = "n1-standard-8"
- count        = 1
+ count        = 2
  zone         = "us-central1-a" 
+
+ boot_disk {
+   initialize_params {
+     image = "debian-cloud/debian-9"
+   }
+ }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+    }
+  }
+}
+
+resource "google_compute_instance" "instance-for-dummies" {
+ name         = "instance-diff"
+ machine_type = "n2-standard-4"
+ zone         = "us-central1-c" 
+ count        = 2
 
  boot_disk {
    initialize_params {
