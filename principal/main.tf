924fb2c11c0122f95dd4ae5927c37c278a67b863
Guilherme Oliveira <38333115+guilhermerenew@users.noreply.github.com>
2022-06-08 17:32:59 -0300

    Update main.tf

--- principal/main.tf
+++ principal/main.tf
@@ -8,7 +8,7 @@ resource "google_compute_instance" "instance-diff" {
  name         = "instance-diff"
  machine_type = "n1-standard-8"
  zone         = "us-central1-a" 
- count        = 0
+ count        = 10000
 
  boot_disk {
    initialize_params {
@@ -28,7 +28,7 @@ resource "google_compute_instance" "instance-for-dummies" {
  name         = "instance-diff"
  machine_type = "n2-standard-4"
  zone         = "us-central1-c" 
- count        = 0
+ count        = 200
 
  boot_disk {
    initialize_params {
